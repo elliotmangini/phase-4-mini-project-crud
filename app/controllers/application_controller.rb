d5558df53d42e77aaadb27e9d3b43bc1e31ed483
Elliot Mangini <elliot.mangini@gmail.com>
2022-10-12 11:02:40 -0600

    commit tree 91b7097287a26d83e76c80f811cbb236d0a20500

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,55 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
 
+  # GET /spices
+  def index
+    spices = Spice.all
+    render json: spices
+  end
+
+  # # GET /spices/:id
+  # def show
+  #   spice = find_spice
+  #   render json: spice
+  # rescue ActiveRecord::RecordNotFound
+  #   render_not_found
+  # end
+
+  # POST /spices
+  def create
+    spice = Spice.create(spice_params)
+    render json: spice, status: :created
+  end
+
+  def update
+    spice = find_spice
+    spice.update(spice_params)
+    render json: spice
+  rescue ActiveRecord::RecordNotFound
+    render_not_found
+  end
+
+  def destroy
+    spice = find_spice
+    spice.destroy
+    head :no_content
+  rescue ActiveRecord::RecordNotFound
+    render_not_found
+  end
+
+
+  private
+
+  def spice_params
+    params.permit(:title, :image, :description, :notes, :rating)
+  end
+
+  def find_spice
+    Spice.find_by(id: params[:id])
+  end
+
+  def render_not_found
+    render json: { error: "Spice not found."}, status: :render_not_found
+  end
+
 end
